bfbf0fd5ed767e712452821076c72f51a120045b
Robinho <robson13.bsantos@gmail.com>
2026-03-02 23:09:17 -0300

    update 02.03.2026

--- src/app/pages/criar-pensamento/criar-pensamento.ts
+++ src/app/pages/criar-pensamento/criar-pensamento.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { PensamentoModel } from '../../models/pensamentoModel';
 import { Services } from '../../core/services';
@@ -15,7 +15,7 @@ export class CriarPensamento {
 
     private service = inject( Services );
     private router = inject( Router );
-    // private formBuilder = inject( FormBuilder );
+    private formBuilder = inject( FormBuilder );
 
     pensamento: PensamentoModel = {
         conteudo: '',
@@ -23,19 +23,68 @@ export class CriarPensamento {
         modelo: ''
     }
 
+    formulario = this.formBuilder.group( {
+        conteudo: [ '', [ Validators.required, Validators.minLength( 3 ), Validators.pattern( /\S+/ ) ] ],
+        autoria: [ '', Validators.required ],
+        modelo: [ 'modelo1', [ Validators.required, Validators.pattern( /\S+/ ) ] ]
+    } )
+
+    get conteudo () {
+        return this.formulario.get( 'conteudo' );
+    }
+
+    get autoria () {
+        return this.formulario.get( 'autoria' );
+    }
+    get modelo () {
+        return this.formulario.get( 'modelo' );
+    }
+
     //formulario?: FormGroup;
     //formulario!: FormGroup;
     //  formulario: FormGroup = new FormGroup( {} );
 
+    // criarPensamento () {
+    //     this.service.criar( this.pensamento ).subscribe( {
+    //         next: () => {
+    //             console.log( 'Pensamento criado com sucesso!' );
+    //             this.router.navigate( [ '/listarPensamento' ] ); // navega só depois do POST
+    //         },
+    //         error: ( err ) => console.error( 'Erro ao criar pensamento:', err )
+    //     } )
+    // }
+
+
     criarPensamento () {
-        this.service.criar( this.pensamento ).subscribe( {
-            next: () => {
-                console.log( 'Pensamento criado com sucesso!' );
-                this.router.navigate( [ '/listarPensamento' ] ); // navega só depois do POST
-            },
-            error: ( err ) => console.error( 'Erro ao criar pensamento:', err )
-        } )
+        if ( this.formulario.valid ) {
+
+            const novoPensamento: PensamentoModel = {
+                conteudo: this.formulario.value.conteudo ?? '',
+                autoria: this.formulario.value.autoria ?? '',
+                modelo: this.formulario.value.modelo ?? ''
+            };
+
+            this.service.criar( novoPensamento ).subscribe( {
+                next: () => {
+                    console.log( 'Pensamento criado com sucesso!' );
+                    this.router.navigate( [ '/listarPensamento' ] );
+                },
+                error: ( err ) => console.error( 'Erro ao criar pensamento:', err )
+            } );
+
+        } else {
+            this.formulario.markAllAsTouched();
+        }
     }
+    enviar () {
+        if ( this.formulario.valid ) {
+            console.log( this.formulario.value );
+        } else {
+            this.formulario.markAllAsTouched();
+        }
+    }
+
+
 
     cancelar () {
         // this.router.navigate( [ '/listarPensamento' ] );
